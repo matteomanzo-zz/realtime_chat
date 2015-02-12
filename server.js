934f26b6c1607b9a3c7e1acf8b55336a34919591
matteomanzo <imatteo.manzo@gmail.com>
2015-02-12 12:50:48 +0000

    chat working

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var express = require('express');
+var io = require('socket.io')(http);
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-	response.send('<h1>Chat!</h1>');
+	response.render('index');
+});
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+		socket.on('chat message', function(msg) {
+			console.log('message: ' + msg);
+		});
+    socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
 });
 
 http.listen(8888, function() {
